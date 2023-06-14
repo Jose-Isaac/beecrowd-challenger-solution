bfb4467a0d9f6acc2ddb71bb529f788775a5b099
Jose Isaac <joseisaacbarreirocampos@gmail.com>
2023-06-14 15:16:18 -0300

    submit challenger 1240 solution

--- 1240/kotlin/src/main/kotlin/Main.kt
+++ 1240/kotlin/src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
-fun main(args: Array<String>) {
+import java.util.Scanner
 
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val inputsSize = scanner.nextInt()
+
+    for (it in 1..inputsSize) {
+        val value = scanner.nextInt()
+        val valueToCheck = scanner.nextInt()
+
+        val isSubValue = if (valueToCheck > value) {
+            false
+        } else {
+            checkSecondValueIsLastSubValue(value.toString(), valueToCheck.toString())
+        }
+
+        println(if (isSubValue) "encaixa" else "nao encaixa")
+    }
+}
+
+fun checkSecondValueIsLastSubValue(value: String, valueToCheck: String): Boolean {
+    val subSequenceForValue = value.subSequence(
+        startIndex = value.length - valueToCheck.length,
+        endIndex = value.length
+    )
+
+    return subSequenceForValue == valueToCheck
 }
---
+++ 1240/kotlin/src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,23 @@
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.comparables.shouldBeEqualComparingTo
+
+class MainKtTest : ShouldSpec({
+
+    should("check a valid sub value") {
+        val value = 5678690
+        val validSubValue = 78690
+
+        val response = checkSecondValueIsLastSubValue(value.toString(), validSubValue.toString())
+
+        response shouldBeEqualComparingTo true
+    }
+
+    should("check a invalid sub value") {
+        val value = 5678690
+        val validSubValue = 567
+
+        val response = checkSecondValueIsLastSubValue(value.toString(), validSubValue.toString())
+
+        response shouldBeEqualComparingTo false
+    }
+})
