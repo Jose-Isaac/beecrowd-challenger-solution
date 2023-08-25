b735ba175cd1d55d64bd51e1396af56859a402bd
Jose Isaac <joseisaacbarreirocampos@gmail.com>
2023-08-25 20:35:52 -0300

    resolved 1047 challenger

--- 1047/kotlin/src/main/kotlin/Main.kt
+++ 1047/kotlin/src/main/kotlin/Main.kt
@@ -1,9 +1,36 @@
-fun main(args: Array<String>) {
-    val inputsSize = readLine()!!.toInt()
-    val inputValues = mutableListOf<Pair<Int, Int>>()
+import kotlin.math.roundToInt
 
-    repeat(inputsSize) {
-        val (value, valueToCheck) = readLine()!!.split(" ").map { it.toInt() }
-        inputValues.add(value to valueToCheck)
+fun main(args: Array<String>) {
+    val (startHour, startMinute, endHour, endMinute) = readLine()!!
+        .split(" ")
+        .map { it.toInt() }
+
+    println(calculate(startHour, startMinute, endHour, endMinute))
+}
+
+fun calculate(startHour: Int, startMinute: Int, endHour: Int, endMinute: Int): String {
+    val MINUTES_PER_HOUR = 60
+    val MINUTES_PER_DAY = 1440
+    val HOURS_PER_DAY = 24
+
+    val startMinutesDuration = startHour * MINUTES_PER_HOUR + startMinute
+    val endMinutesDuration = endHour * MINUTES_PER_HOUR + endMinute
+
+    val totalMinutesPerDay = if (
+        endHour <= startHour &&
+        endMinute <= startMinute
+    ) {
+        MINUTES_PER_DAY
+    } else {
+        0
     }
+
+    val totalDifferenceOfMinutes = totalMinutesPerDay + endMinutesDuration - 0 - startMinutesDuration
+
+    val duration = totalDifferenceOfMinutes.toFloat() / MINUTES_PER_DAY
+
+    val hours = duration * HOURS_PER_DAY
+    val minutes = (hours - hours.toInt()) * MINUTES_PER_HOUR
+
+    return "O JOGO DUROU ${hours.toInt()} HORA(S) E ${minutes.roundToInt()} MINUTO(S)"
 }
